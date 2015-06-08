06210e8f47a3de015107f4564c97c25ada7e8658
arash-t <a.tavakolie@fisheries.ubc.ca>
2015-06-08 13:39:22 -0700

    tbl.AllocationResult now contains tbl.AllocationResult_L3, hence no need for separate inserts

--- Aggregations/Create_summaries.sql
+++ Aggregations/Create_summaries.sql
@@ -6,12 +6,8 @@ delete from  MerlinAgg.dbo.Data
 
 insert into MerlinAgg.dbo.Data
 select d.*, 1466 as UnitPrice --- 1466 is the default price
-from [dbo].[Data_verfied_L1L2_Ver37] d
+from [dbo].[Data] d
 
-insert into MerlinAgg.dbo.Data
-select d.*, 1466 as UnitPrice
-from [dbo].[Data_verified_layer3_all] d
-Go
 
 -- update price
 update MerlinAgg.dbo.Data
@@ -23,16 +19,10 @@ from MerlinAgg.dbo.Data d inner join merlin.dbo.Price p on d.[year] = p.[Year] A
 ---EEZs/High seas ---------------------------------------------------------------------------
 USE Merlin
 drop table MerlinAgg.dbo.AllocationResult_EEZ 
---L1L2 
+
 SELECT distinct r.UniversalDataID, a.InheritedAtt_BelongsToReconstructionEEZID as EEZID, a.FaoAreaID, sum(r.AllocatedCatch) as TotalCatch
   into MerlinAgg.dbo.AllocationResult_EEZ
   FROM [dbo].[AllocationResult] r inner join [dbo].[AllocationSimpleArea] a on r.AllocationSimpleAreaID = a.AllocationSimpleAreaID
-  group by r.UniversalDataID, a.InheritedAtt_BelongsToReconstructionEEZID , a.FaoAreaID
-
---L3
-INSERT into MerlinAgg.dbo.AllocationResult_EEZ
-SELECT distinct r.UniversalDataID, a.InheritedAtt_BelongsToReconstructionEEZID as EEZID, a.FaoAreaID, sum(r.AllocatedCatch) as TotalCatch 
-  FROM [dbo].[AllocationResult_L3] r inner join [dbo].[AllocationSimpleArea] a on r.AllocationSimpleAreaID = a.AllocationSimpleAreaID
   group by r.UniversalDataID, a.InheritedAtt_BelongsToReconstructionEEZID , a.FaoAreaID
 
 ---------------------------------------------------------------------------------------------
@@ -41,7 +31,6 @@ SELECT distinct r.UniversalDataID, a.InheritedAtt_BelongsToReconstructionEEZID a
 
 USE Merlin
 drop table MerlinAgg.dbo.AllocationResult_LME
---L1L2 
 
 select r.UniversalDataID ,cr.AreaID As LMEID, d.DataLayerID, d.FishingEntityID, d.InputTypeID, d.SectorTypeID, d.TaxonKey, d.Year,  sum(r.AllocatedCatch*cr.WaterArea/(c.WaterArea)) as TotalCatch
 into MerlinAgg.dbo.AllocationResult_LME
@@ -51,19 +40,6 @@ inner join AllocationResult r on r.CellID = cr.CellID
 inner join data d on d.UniversalDataID = r.UniversalDataID
 where MarineLayerID = 3 and c.WaterArea > 0
 group by r.UniversalDataID, cr.AreaID, d.DataLayerID, d.FishingEntityID, d.InputTypeID, d.SectorTypeID, d.TaxonKey, d.Year
-
-
---L3
-use Merlin
-insert into MerlinAgg.dbo.AllocationResult_LME
-select r.UniversalDataID ,cr.AreaID As LMEID, d.DataLayerID, d.FishingEntityID, d.InputTypeID, d.SectorTypeID, d.TaxonKey, d.Year,  sum(r.AllocatedCatch*cr.WaterArea/(c.WaterArea)) as TotalCatch
-from SimpleAreaCellAssignmentRaw cr 
-inner join Cell c on c.CellID = cr.CellID 
-inner join AllocationResult_L3 r on r.CellID = cr.CellID
-inner join [dbo].[Data_verified_layer3_all] d on d.UniversalDataID = r.UniversalDataID
-where MarineLayerID = 3 and c.WaterArea > 0
-group by r.UniversalDataID, cr.AreaID, d.DataLayerID, d.FishingEntityID, d.InputTypeID, d.SectorTypeID, d.TaxonKey, d.Year
-
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------
 
